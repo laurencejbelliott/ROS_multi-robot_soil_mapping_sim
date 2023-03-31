2344af72c95f149a21ac06639e7e2d0e0b493101
Laurence Roberts-Elliott <laurencejbelliott@gmail.com>
2023-03-31 11:54:30 +0100

    Seperate node to stop after time elapsed

--- stage_soil_mapping_mrs/scripts/run_trials.py
+++ stage_soil_mapping_mrs/scripts/run_trials.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from os.path import exists
 import roslaunch
 import rospy
 import itertools
@@ -16,35 +17,7 @@ num_robots = 3
 ta_algo = "SSI"
 sampling_algo = "random"
 env_crop_factor = 1 # Environment cropping is not yet implemented
-
-# cli_args = [package_path+'/launch/simplified_nav_'+str(num_robots)+'_rob_compaction_field.launch',
-#             'trial_num:='+str(trial_num),
-#             'num_robots:='+str(num_robots),
-#             'ta_algo:="'+ta_algo+'"',
-#             'sampling_algo:="'+sampling_algo+'"',
-#             'env_crop_factor:=1',
-#             'bag_name:="'+str(num_robots)+'_robs_'+str(ta_algo)+'_'+str(sampling_algo)+'_'+str(trial_num)+'"'
-#             ]
-# roslaunch_args = cli_args[1:]
-# roslaunch_file = [(roslaunch.rlutil.resolve_launch_arguments(cli_args)[0], roslaunch_args)]
-
-# # uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-
-# parent = ROSLaunchParent("run_trials", roslaunch_file, is_core=True)     # run_id can be any string
-# parent.start()
-
-# rospy.init_node('run_trials', anonymous=True)
-
-# while not rospy.is_shutdown():
-#     # Wait for /sampling_time_budget_elapsed to be published as True
-#     sampling_completed = False
-#     try:
-#         sampling_completed = rospy.wait_for_message('/sampling_time_budget_elapsed', Bool, timeout=1).data
-#     except rospy.ROSException:
-#         pass
-#     if sampling_completed:
-#         parent.shutdown()
-#         break
+sampling_time_budget = 240 # seconds, as in Mesa sim trials
 
 # Define sets of parameters to combine
 trial_num_set = set([i for i in range(1, 11)])
@@ -56,7 +29,8 @@ print("Set of number of robots: ", num_robots_set)
 ta_algo_set = set(['RR', 'SSI'])
 print("Set of task allocation algorithms: ", ta_algo_set)
 
-sampling_algo_set = set(["grid", "random"])
+# sampling_algo_set = set(["grid", "random"])
+sampling_algo_set = set(["random"]) # No 'grid' sampling in Mesa trials
 print("Set of sampling algorithms: ", sampling_algo_set)
 
 env_crop_factor_set = set([1])
@@ -66,16 +40,25 @@ print("Set of environment cropping factors: ", env_crop_factor_set)
 combinations = list(itertools.product(trial_num_set, num_robots_set, ta_algo_set, sampling_algo_set, env_crop_factor_set))
 # Remove combinations that have no trial_num, num_robots, ta_algo, sampling_algo, env_crop_factor
 combinations = [i for i in combinations if i[0] != i[1] != i[2] != i[3] != i[4]]
+total_trials = len(combinations)
 
-
-print("Set of trial numbers, num_robots, ta_algo, sampling_algo, env_crop_factor combinations:\n", combinations)
-
+print("\nRunning simulated trials...", combinations)
+trial_count = 0
 for trial_params in combinations:
+    trial_count += 1
     trial_num = trial_params[0]
     num_robots = trial_params[1]
     ta_algo = trial_params[2]
     sampling_algo = trial_params[3]
     env_crop_factor = trial_params[4]
+
+    # Check if trial has already run by checking if rosbag file exists
+    bag_name = str(num_robots)+'_robs_'+str(ta_algo)+'_'+str(sampling_algo)+'_'+str(trial_num)
+    if exists(package_path+'/bags/'+bag_name+'.bag'):
+        print("Trial ", bag_name," (", trial_count, "/", total_trials,") has already been run, skipping...")
+        continue
+
+    print("Running trial ", trial_count, "/", total_trials, ": ", trial_params)
 
     cli_args = [package_path+'/launch/simplified_nav_'+str(num_robots)+'_rob_compaction_field.launch',
                 'trial_num:='+str(trial_num),
@@ -83,7 +66,8 @@ for trial_params in combinations:
                 'ta_algo:="'+ta_algo+'"',
                 'sampling_algo:="'+sampling_algo+'"',
                 'env_crop_factor:=1',
-                'bag_name:="'+str(num_robots)+'_robs_'+str(ta_algo)+'_'+str(sampling_algo)+'_'+str(trial_num)+'"'
+                'bag_name:="'+bag_name+'"',
+                'sampling_time_budget:='+str(sampling_time_budget)
                 ]
     roslaunch_args = cli_args[1:]
     roslaunch_file = [(roslaunch.rlutil.resolve_launch_arguments(cli_args)[0], roslaunch_args)]
