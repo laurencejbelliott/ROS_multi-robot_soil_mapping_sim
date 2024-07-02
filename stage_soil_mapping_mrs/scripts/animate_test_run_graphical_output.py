1b4994b01717c81b14d0a3df6b1bb3ddcf699383
Laurence Roberts-Elliott <laurencejbelliott@gmail.com>
2024-07-02 15:22:44 +0100

    Script to generate GIFs of heatmaps and trajectories for test run to aid debugging

---
+++ stage_soil_mapping_mrs/scripts/animate_test_run_graphical_output.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# This script is used to animate the ROSbags.
+__author__ = "Laurence Roberts-Elliott"
+# Import modules
+import os
+import glob
+import imageio
+from rospkg import RosPack
+from animate_graphical_output import convert_to_gif
+
+
+if __name__ == '__main__':
+    # Get path of figures directory
+    rp = RosPack()
+    package_path = rp.get_path('stage_soil_mapping_mrs')
+
+    figures_path = package_path + '/figures/test_run'
+    print(figures_path)
+
+    # Animate kriging interpolation
+    path_to_png_files = figures_path + '/interpolation_raw/'
+
+    png_files = glob.glob(path_to_png_files + '*.png')
+
+    convert_to_gif(path_to_png_files,
+                figures_path+'/interpolation_animation.gif')
+    
+    # Animate kriging variance
+    path_to_png_files = figures_path + '/variance_raw/'
+
+    png_files = glob.glob(path_to_png_files + '*.png')
+
+    convert_to_gif(path_to_png_files,
+                    figures_path+'/variance_animation.gif')
